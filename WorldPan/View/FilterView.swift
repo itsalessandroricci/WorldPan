b4dce07acbb7682b52cce3fe78904dd957324eca
D4fra <148436310+D4fra@users.noreply.github.com>
2023-10-19 12:45:11 +0200

    I add the FilterView

---
+++ WorldPan/View/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterView.swift
+//  WorldPan
+//
+//  Created by Davide Formisano on 19/10/23.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterView()
+}
