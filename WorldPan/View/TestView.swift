c8c65fb3324ab5152d4ef9c41fd522895f466d6a
itsalessandroricci <alessandro.ricci.rcc@gmail.com>
2023-10-21 18:44:39 +0200

    Final update of recipe view and creation of views for ingredients and instructions

---
+++ WorldPan/View/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  WorldPan
+//
+//  Created by Alessandro Ricci on 21/10/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
